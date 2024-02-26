a6750ff1b1842c5c3637644d51b79c271106710a
Arshad Qadir <coolarshad@Arshads-Mini.domain.name>
2024-02-27 01:02:40 +0545

    email configured

--- home/views.py
+++ home/views.py
@@ -3,7 +3,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
-from django.contrib import messages
+import smtplib
+from email.mime.text import MIMEText
 
 def submit_contact_form(request):
     if request.method == 'POST':
@@ -11,26 +12,49 @@ def submit_contact_form(request):
         email = request.POST.get('email')
         subject = request.POST.get('subject')
         message = request.POST.get('message')
-
+        password = "[~9K[]X~PFZ+"
         email_content = f"Name: {name}\nEmail: {email}\nSubject: {subject}\nMessage: {message}"
 
         # Replace these with your actual email addresses
-        sender_email = email
-        recipient_email = ''
+        sender_email = 'operations@platinum-my.com'
+        recipient_email = ['operations@platinum-my.com','developer@noticevalley.com']
 
         try:
-            send_mail(
-                subject,
-                email_content,
-                sender_email,
-                [recipient_email],
-                fail_silently=False,
-            )
-            messages.success(request, "Email sent successfully!")
+            # send_mail(
+            #     subject,
+            #     email_content,
+            #     'developer@noticevalley.com',
+            #     ['developer@noticevalley.com'],
+            #     fail_silently=False,
+            # )
+            send_email(subject, email_content, sender_email, recipient_email, password)
+            
             return HttpResponse("Email sent successfully!")
         except Exception as e:
-            messages.error(request, f"Error: {e}")
             return HttpResponse(f"Error: {e}")
     else:
         # Handle other HTTP methods if needed
         return HttpResponse(status=405)  # Method not allowed
+
+
+
+
+# subject = "Email Subject"
+# body = "This is the body of the text message"
+# sender = "developer@noticevalley.com"
+# recipients = ["coolarshad96@gmail.com", "developer@noticevalley.com"]
+# password = "Coolarshad1@"
+
+
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    with smtplib.SMTP_SSL('sxb1plzcpnl487536.prod.sxb1.secureserver.net', 465) as smtp_server:
+       smtp_server.login(sender, password)
+       smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Message sent!")
+
+
+# send_email(subject, body, sender, recipients, password)

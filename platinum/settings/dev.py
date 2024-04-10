a48ccbd51f9abc089fa7c92c6f8bb070b2a2d9b9
Arshad Qadir <coolarshad@Arshads-Mini.domain.name>
2024-04-10 23:14:26 +0545

    bug fixed with css

--- platinum/settings/dev.py
+++ platinum/settings/dev.py
@@ -8,7 +8,7 @@ SECRET_KEY = "django-insecure-2ob+qr3+fjhaww=kds0*__=-=rt=s+!lp)@sb4og&g3qaibna!
 
 # SECURITY WARNING: define the correct hosts in production!
 # ALLOWED_HOSTS = ["platinum-my.com","97.74.82.132"]
-ALLOWED_HOSTS = ["noticevalley.com"]
+ALLOWED_HOSTS = []
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
